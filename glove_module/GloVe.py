6e84c934f4bbd7138414c04429cbeed0ebec0d43
Veselovskiy Sergey <veselovskiysergey94@gmail.com>
2017-12-30 18:00:33 +0300

    refactor

--- glove_module/GloVe.py
+++ glove_module/GloVe.py
@@ -34,32 +34,25 @@ class GloveModule:
             with self.graph.device("/cpu:0"):
                 count_max = tf.constant([config.COUNT_MAX], dtype=tf.float32)  # tf константа -
                 alpha = tf.constant([config.SCALING_FACTOR], dtype=tf.float32)  # tf константа -
-                self.focal_input = tf.placeholder(tf.int32, shape=[config.BATCH_SIZE])  # tf переменная -
-                self.context_input = tf.placeholder(tf.int32, shape=[config.BATCH_SIZE])  # tf переменная -
+                self.row_input = tf.placeholder(tf.int32, shape=[config.BATCH_SIZE])  # tf переменная -
+                self.coloumn_input = tf.placeholder(tf.int32, shape=[config.BATCH_SIZE])  # tf переменная -
                 self.cooccurrence_count = tf.placeholder(tf.float32, shape=[config.BATCH_SIZE])  # tf переменная -
                 self.embeddings = tf.Variable(
                     tf.random_uniform([vocab_size, config.EMBEDDING_SIZE], 1.0, -1.0)
                 )
-                # focal_embeddings = tf.Variable(
-                #     tf.random_uniform([vocab_size, config.EMBEDDING_SIZE], 1.0, -1.0)
-                # )
 
-                # context_embeddings = tf.Variable(
-                #     tf.random_uniform([vocab_size, config.EMBEDDING_SIZE], 1.0, -1.0)
-                # )
-
-                focal_biases = tf.Variable(
+                row_biases = tf.Variable(
                     tf.random_uniform([vocab_size], 1.0, -1.0)
                 )
 
-                context_biases = tf.Variable(
+                coloumn_biases = tf.Variable(
                     tf.random_uniform([vocab_size], 1.0, -1.0)
                 )
 
-                focal_embedding = tf.nn.embedding_lookup([self.embeddings], self.focal_input)
-                context_embedding = tf.nn.embedding_lookup([self.embeddings], self.context_input)
-                focal_bias = tf.nn.embedding_lookup([focal_biases], self.focal_input)
-                context_bias = tf.nn.embedding_lookup([context_biases], self.context_input)
+                row_embedding = tf.nn.embedding_lookup([self.embeddings], self.row_input)
+                coloumn_embedding = tf.nn.embedding_lookup([self.embeddings], self.coloumn_input)
+                row_bias = tf.nn.embedding_lookup([row_biases], self.row_input)
+                coloumn_bias = tf.nn.embedding_lookup([coloumn_biases], self.coloumn_input)
 
                 weighting_factor = tf.minimum(
                     1.0,
@@ -69,44 +62,33 @@ class GloveModule:
                     )
                 )
 
-                embedding_product = tf.reduce_sum(tf.multiply(focal_embedding, context_embedding), 1)
+                embedding_product = tf.reduce_sum(tf.multiply(row_embedding, coloumn_embedding), 1)
 
                 log_cooccurrences = tf.log(tf.to_float(self.cooccurrence_count))
 
-                distance_expr = tf.square(tf.add_n([
+                distance = tf.square(tf.add_n([
                     embedding_product,
-                    focal_bias,
-                    context_bias,
+                    row_bias,
+                    coloumn_bias,
                     tf.negative(log_cooccurrences)
                 ]))
 
-                single_losses = tf.multiply(weighting_factor, distance_expr)
-                self.total_loss = tf.reduce_sum(single_losses)
+                losses = tf.multiply(weighting_factor, distance)
+                self.result_loss = tf.reduce_sum(losses)
                 # with self.graph.device("/cpu:0"):
-                tf.summary.scalar('loss', self.total_loss)
+                tf.summary.scalar('loss', self.result_loss)
                 # self.optimizer = tf.train.AdamOptimizer(config.LEARNING_RATE).minimize(self.total_loss)
-                self.optimizer = tf.train.AdagradOptimizer(config.LEARNING_RATE).minimize(self.total_loss)
+                self.optimizer = tf.train.AdagradOptimizer(config.LEARNING_RATE).minimize(self.result_loss)
         print("Done creating")
 
     def begin(self):
         print("Prepare cooccurrence matrix...")
-
-        #i_indices, j_indices, counts = zip(*self.cooccurrence_matrix)
-        print("Done preparing")
-        print("Get batches...")
-        #self.batches = list(self.batchify(i_indices, j_indices, cooc))
-        print("Done get batches.")
-        print("Begin training: {}".format(datetime.datetime.now().time()))
-        print("=================")
         sys.stdout.flush()
         with tf.Session(graph=self.graph) as session:
             merged = tf.summary.merge_all()
             batch_writer = tf.summary.FileWriter('./logs/batch')
-            tf.initialize_all_variables().run()
+            tf.global_variables_initializer().run()
             for epoch in range(config.NUM_EPOCHS):
-                #shuffle(self.batches)
-                print("Batches shuffled")
-                print("-----------------")
                 sys.stdout.flush()
                 accumulated_loss = 0
                 total = len(self.cooccurrence_matrix)
@@ -116,24 +98,21 @@ class GloveModule:
                     batch_index += 1
                     ww, counts = zip(*self.cooccurrence_matrix[i:i+config.BATCH_SIZE])
                     i_s, j_s = zip(*ww)
-                    #i_s, j_s, counts = batch
                     if len(counts) != config.BATCH_SIZE:
                         continue
-                    feed_dict = {self.focal_input: i_s, self.context_input: j_s, self.cooccurrence_count: counts}
+                    feed_dict = {self.row_input: i_s, self.coloumn_input: j_s, self.cooccurrence_count: counts}
                     summaries, _, total_loss_, = session.run(
-                        [merged, self.optimizer, self.total_loss], feed_dict=feed_dict)
+                        [merged, self.optimizer, self.result_loss], feed_dict=feed_dict)
                     accumulated_loss += total_loss_
-                    if (batch_index + 1) % config.REPORT_BATCH_SIZE == 0:
+                    if (batch_index + 1) % config.LOG_BATCH_SIZE == 0:
                         print("Epoch: {0}/{1}".format(epoch + 1, config.NUM_EPOCHS))
                         print("Batch: {0}/{1}".format(batch_index + 1, num_batches))
-                        print("Average loss: {}".format(accumulated_loss / config.REPORT_BATCH_SIZE))
-                        print("-----------------")
+                        print("Average loss: {}".format(accumulated_loss / config.LOG_BATCH_SIZE))
+                        print("___________________________")
                         batch_writer.add_summary(summaries, epoch * num_batches + batch_index)
                         sys.stdout.flush()
                         accumulated_loss = 0
                 print("Epoch finished: {}".format(datetime.datetime.now().time()))
-                print("=================")
-
                 sys.stdout.flush()
             batch_writer.close()
             final_embeddings = self.embeddings.eval()
